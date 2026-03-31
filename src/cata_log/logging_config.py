ec3ed7c5c6e36ee9b541e54d1a89bc0faba118d9
david <no@email>
2026-04-01 01:22:51 +0200

    feat: extend logging to file

--- src/cata_log/logging_config.py
+++ src/cata_log/logging_config.py
@@ -3,11 +3,34 @@ import logging.config
 from .constants import LOG_DIRECTORY_PATH, DefaultConfig
 from .utils.shortcuts import get_config
 
+COMMON_FILEHANDLER_CONFIG = {
+    "level": "DEBUG",
+    "class": "logging.handlers.RotatingFileHandler",
+    "maxBytes": int(
+        get_config(DefaultConfig.log_file_maxsize.name),
+    ),
+    "backupCount": int(get_config(DefaultConfig.log_file_backup_count.name)),
+    "formatter": "json",
+}
+
+COMMON_LOGGER_CONFIG = {
+    "level": getattr(
+        logging,
+        get_config(DefaultConfig.log_level.name),
+        "INFO",
+    ),
+    "propagate": True,
+}
+
 LOGGING_CONFIG = {
     "version": 1,
     "disable_existing_loggers": False,
     "formatters": {
-        "default": {
+        "console": {
+            "format": "{asctime} ({levelname}) - {name}: {module}.{funcName}:{lineno} - {message}",
+            "style": "{",
+        },
+        "json": {
             "()": "pythonjsonlogger.json.JsonFormatter",
             "format": "{asctime}{levelname}{name}{module}{funcName}{lineno}{message}",
             "style": "{",
@@ -17,26 +40,47 @@ LOGGING_CONFIG = {
         "console": {
             "level": "DEBUG",
             "class": "logging.StreamHandler",
-            "formatter": "default",
+            "formatter": "console",
         },
-        "logfile": {
-            "level": "DEBUG",
-            "class": "logging.handlers.RotatingFileHandler",
+        "root-logfile": {
+            "filename": str(LOG_DIRECTORY_PATH / "root.log"),
+            **COMMON_FILEHANDLER_CONFIG,
+        },
+        "cata-log-logfile": {
             "filename": str(LOG_DIRECTORY_PATH / "cata-log.log"),
-            "maxBytes": int(
-                get_config(DefaultConfig.log_file_maxsize.name),
-            ),
-            "backupCount": int(get_config(DefaultConfig.log_file_backup_count.name)),
-            "formatter": "default",
+            **COMMON_FILEHANDLER_CONFIG,
+        },
+        "amqp-logfile": {
+            "filename": str(LOG_DIRECTORY_PATH / "amqp.log"),
+            **COMMON_FILEHANDLER_CONFIG,
+        },
+        "celery-logfile": {
+            "filename": str(LOG_DIRECTORY_PATH / "celery.log"),
+            **COMMON_FILEHANDLER_CONFIG,
+        },
+        "sqlalchemy-logfile": {
+            "filename": str(LOG_DIRECTORY_PATH / "sqlalchemy.log"),
+            **COMMON_FILEHANDLER_CONFIG,
+        },
+        "uvicorn-logfile": {
+            "filename": str(LOG_DIRECTORY_PATH / "uvicorn.log"),
+            **COMMON_FILEHANDLER_CONFIG,
         },
     },
     "root": {
-        "handlers": ["console", "logfile"],
+        "handlers": ["console", "root-logfile"],
         "level": getattr(
             logging,
             get_config(DefaultConfig.log_level.name),
             "INFO",
         ),
+    },
+    "loggers": {
+        "uvicorn": {"handlers": ["uvicorn-logfile"], **COMMON_LOGGER_CONFIG},
+        "celery": {"handlers": ["celery-logfile"], **COMMON_LOGGER_CONFIG},
+        "sqlalchemy": {"handlers": ["sqlalchemy-logfile"], **COMMON_LOGGER_CONFIG},
+        "amqp": {"handlers": ["amqp-logfile"], **COMMON_LOGGER_CONFIG},
+        "cata_log": {"handlers": ["cata-log-logfile"], **COMMON_LOGGER_CONFIG},
     },
 }
 
