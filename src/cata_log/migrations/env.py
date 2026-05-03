99c219dfccb4e696eb41fbde6f654e48595008e8
david <no@email>
2026-05-03 14:48:30 +0200

    fix (migrations): add initial migration

--- src/cata_log/migrations/env.py
+++ src/cata_log/migrations/env.py
@@ -16,6 +16,9 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+import sys
+
+sys.path.insert(0, "src/")
 from logging.config import fileConfig
 
 from alembic import context
